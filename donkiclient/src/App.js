3587514eb3bd34bbfe93f77fe1699553635b4731
hwchw316 <hwchw316@gmail.com>
2021-03-14 10:06:42 +0000

    making a title bar

--- donkiclient/src/App.js
+++ donkiclient/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Container , AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import imgurllinks from './images/icon1.jpg'
 
 const App = ()=> {
 return(
-    <div>
-        App.js wokring
-    </div>
+    <Container maxwidth ='lg'>
+        <AppBar position = 'static' color = 'inherit'>
+            <Typography variant = 'h2' align = 'center '> Don Don Donki </Typography>
+            <img src = {imgurllinks} alt= "imgurllinks" height = '60' />
+        </AppBar>
+       
+    </Container>
 );
 }
 
