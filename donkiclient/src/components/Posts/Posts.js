d751df29b7b099325c7645144c95145721393b40
hwchw316 <hwchw316@gmail.com>
2021-03-14 16:48:08 +0000

    fixed bugs

--- donkiclient/src/components/Posts/Posts.js
+++ donkiclient/src/components/Posts/Posts.js
@@ -3,6 +3,8 @@ import Post from './Post/Post'
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
 
+
+
 const Posts = () => {
     const posts = useSelector ((state) => state.posts);
     const classes = useStyles();
--- donkiclient/src/reducers/posts.js
+++ donkiclient/src/reducers/posts.js
@@ -1,12 +1,12 @@
 import Posts from "../components/Posts/Posts";
 
-export default (post = [], action) => {
+export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
                 return action.payload;
         case 'CREATE':
             return posts;
         default:
-            return Posts;
+            return posts;
     }
 }
