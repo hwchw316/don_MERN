27f371f5400a72b37af3dde7e57a8b9dafb3549f
hwchw316 <hwchw316@gmail.com>
2021-03-14 10:16:27 +0000

    testing coonpoments

--- donkiclient/src/App.js
+++ donkiclient/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Container , AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
+import Form  from './components/Form/Form'
+import Posts  from './components/Posts/Posts'
 import imgurllinks from './images/icon1.jpg'
 
 const App = ()=> {
@@ -11,7 +13,22 @@ return(
             <Typography variant = 'h2' align = 'center '> Don Don Donki </Typography>
             <img src = {imgurllinks} alt= "imgurllinks" height = '60' />
         </AppBar>
-       
+
+        <Grow in> 
+            <Container>
+                <Grid container justify = 'space-between' alignItems = 'stretch' spacing ={3}>
+                    <Grid item xs = {12} sm ={7} >
+                        <Posts/>
+                    </Grid>
+
+                    <Grid item xs = {12} sm ={4} >
+                        <Form/>
+                    </Grid>
+
+                </Grid>
+            </Container>
+        </Grow>
+
     </Container>
 );
 }
