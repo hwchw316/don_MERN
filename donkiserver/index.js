dbab234a5e7e528b113ead4670f44ae1269aab78
hwchw316 <hwchw316@gmail.com>
2021-03-14 07:47:55 +0000

    server is live on port 5000

--- donkiserver/index.js
+++ donkiserver/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+app.use(bodyParser.json({limit: "30mb", extender : true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extender : true}));
+app.use(cors());
+
+///
+
+const MONGO_URL = 'mongodb+srv://donkininja:donkininja123@cluster0.ztlfx.mongodb.net/Donki_Database?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, ()=> console.log(`Server is live on port : ${PORT}`)))
+    .catch((error)=> console.log(error.message));
+
+    mongoose.set('useFindAndModify',false);
