bf6f7fbf1adf78a40b4d8e8adc0ff8cfa6144b62
hwchw316 <hwchw316@gmail.com>
2021-03-15 16:02:32 +0000

    unable to debug proxy in react

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # don_MERN
+
+https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
+https://www.gitmemory.com/issue/gitpod-io/gitpod/628/503584932
--- donkiclient/src/reducers/index.js
+++ donkiclient/src/reducers/index.js
@@ -3,3 +3,5 @@ import posts from './posts'
 
 export default combineReducers({  posts  });
 // posts: posts
+// something https://www.gitpod.io/blog/gitpodify/#invalid-host-header
+/// npm install webpack-dev-server --save-dev
